0b69efb3f6cb1df3fa389827b69f1c3c86d476fa
Steven <steven@Stevens-MacBook-Air.local>
2026-03-30 13:39:51 +0800

    fix typo

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -2,5 +2,5 @@ module "kms_policy" {
   source     = "../../modules/iam"
   aws_region = local.aws_region
   role_name  = "terraform-prod-role"
-  tags       = locals.common_tags
+  tags       = local.common_tags
 }
