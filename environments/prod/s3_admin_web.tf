dab226a84882083152c005c970e6f643f54a6a0e
Steven <steven@Stevens-MacBook-Air.local>
2026-04-15 02:20:33 +0800

    remove 'move' block

--- environments/prod/s3_admin_web.tf
+++ environments/prod/s3_admin_web.tf
@@ -9,35 +9,3 @@ module "s3_admin_web" {
   cloudfront_distribution_arn = module.cloudfront_admin_web.arn
   tags                        = local.common_tags
 }
-
-# --- Moved blocks (safe to remove after one successful apply) ---
-
-moved {
-  from = aws_s3_bucket.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket.this
-}
-
-moved {
-  from = aws_s3_bucket_ownership_controls.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket_ownership_controls.this
-}
-
-moved {
-  from = aws_s3_bucket_public_access_block.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket_public_access_block.this
-}
-
-moved {
-  from = aws_s3_bucket_server_side_encryption_configuration.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket_server_side_encryption_configuration.this
-}
-
-moved {
-  from = aws_s3_bucket_versioning.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket_versioning.this
-}
-
-moved {
-  from = aws_s3_bucket_policy.admin_web
-  to   = module.s3_admin_web.aws_s3_bucket_policy.this
-}
